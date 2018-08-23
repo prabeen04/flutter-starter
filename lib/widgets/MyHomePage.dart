93fbc2b574118994460b1c701c6ea8fa85acd2ae
Prabeen04 <prabeen.strange@gmail.com>
2018-08-23 23:39:23 +0530

    MyHomePage moved to new file

---
+++ lib/widgets/MyHomePage.dart
@@ -0,0 +1,8 @@
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
