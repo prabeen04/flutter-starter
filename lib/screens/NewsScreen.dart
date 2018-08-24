fd55dc10e87cbfde7316b4a44511b92f1e118393
Prabeen04 <prabeen.strange@gmail.com>
2018-08-24 23:47:03 +0530

    news Screen Text added

---
+++ lib/screens/NewsScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: new Center(
+        child: new Text('News Screen')
+      )
+    );
+  }
+}
