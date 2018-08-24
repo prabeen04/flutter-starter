759231fc78db160697e084190b8746cde7f92089
Prabeen04 <prabeen.strange@gmail.com>
2018-08-24 23:26:29 +0530

    ProfileScreen content added

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -4,7 +4,9 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: new Center(
+        child: new Text('Profile Screen',)
+      )
     );
   }
 }
