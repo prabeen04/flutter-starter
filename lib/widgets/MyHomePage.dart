4c3f9a0e342f30db1d3c1bae89ba930062e4e8c5
Prabeen04 <prabeen.strange@gmail.com>
2018-08-24 23:21:21 +0530

    new Drawer added

--- lib/widgets/MyHomePage.dart
+++ lib/widgets/MyHomePage.dart
@@ -25,13 +25,39 @@ class _MyHomePageState extends State<MyHomePage> {
         title: new Text(widget.title),
       ),
       drawer: new Drawer(
-        child: new ListTile(
-              leading: new Icon(Icons.change_history),
-              title: new Text('Change history'),
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the Drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
               onTap: () {
-                Navigator.pop(context); // close the drawer
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
               },
-            )
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
       body: new Center(
         child: new Column(
