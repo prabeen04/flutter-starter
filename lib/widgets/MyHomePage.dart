c2eef717d008f36f00309e24b92a9a96274086a3
Prabeen04 <prabeen.strange@gmail.com>
2018-08-24 23:14:50 +0530

    code cleaned

--- lib/widgets/MyHomePage.dart
+++ lib/widgets/MyHomePage.dart
@@ -21,10 +21,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      // backgroundColor: Colors.orangeAccent,
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
       drawer: new Drawer(
@@ -32,28 +29,12 @@ class _MyHomePageState extends State<MyHomePage> {
               leading: new Icon(Icons.change_history),
               title: new Text('Change history'),
               onTap: () {
-                // change app state...
                 Navigator.pop(context); // close the drawer
               },
             )
       ),
       body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.network(
@@ -75,7 +56,6 @@ class _MyHomePageState extends State<MyHomePage> {
           alignment: Alignment.center,
           child: new BottomAppBar(
             child: new Row(
-              // alignment: MainAxisAlignment.spaceAround,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 new IconButton(
