91d5f6d7bdaa9d309f05a1b962c82e5fdfd51cab
Prabeen04 <prabeen.strange@gmail.com>
2018-08-24 18:39:06 +0530

    drawer added

--- lib/widgets/MyHomePage.dart
+++ lib/widgets/MyHomePage.dart
@@ -27,6 +27,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
+      drawer: new Drawer(),
       body: new Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
