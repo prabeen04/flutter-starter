887365a7e973b2c23aac90a00959a5d438785b79
Prabeen04 <prabeen.strange@gmail.com>
2018-08-24 23:10:16 +0530

    bottom Navigation bar added

--- lib/widgets/MyHomePage.dart
+++ lib/widgets/MyHomePage.dart
@@ -69,6 +69,49 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      bottomNavigationBar: new Container(
+          color: Colors.white,
+          height: 50.0,
+          alignment: Alignment.center,
+          child: new BottomAppBar(
+            child: new Row(
+              // alignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new IconButton(
+                  icon: Icon(
+                    Icons.home,
+                  ),
+                  onPressed: () {},
+                ),
+                new IconButton(
+                  icon: Icon(
+                    Icons.search,
+                  ),
+                  onPressed: null,
+                ),
+                new IconButton(
+                  icon: Icon(
+                    Icons.add_box,
+                  ),
+                  onPressed: null,
+                ),
+                new IconButton(
+                  icon: Icon(
+                    Icons.favorite,
+                  ),
+                  onPressed: null,
+                ),
+                new IconButton(
+                  icon: Icon(
+                    Icons.account_box,
+                  ),
+                  onPressed: null,
+                ),
+              ],
+            ),
+          ),
+        ),
       floatingActionButton: new FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
