ab98a4935e4068081ae870c2cb512a851b7ea835
Prabeen04 <prabeen.strange@gmail.com>
2018-08-25 19:41:25 +0530

    small stuff

--- lib/widgets/MyHomePage.dart
+++ lib/widgets/MyHomePage.dart
@@ -95,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.network(
               'https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg',
--- lib/widgets/placeholder.dart
+++ lib/widgets/placeholder.dart
@@ -8,7 +8,9 @@ class PlaceholderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Prabeen'),
+      ),
       body: Container(
         color: color,
       ),
