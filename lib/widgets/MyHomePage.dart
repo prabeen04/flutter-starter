754dc5b1c554a1f311336f6583e36fff50a99193
Prabeen04 <prabeen.strange@gmail.com>
2018-08-25 15:09:20 +0530

    Tab bar interactivity added

--- lib/widgets/MyHomePage.dart
+++ lib/widgets/MyHomePage.dart
@@ -13,13 +13,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  int currentIndex = 0;
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
-
+void onTabTapped(int index) {
+   setState(() {
+     _currentIndex = index;
+   });
+ }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,7 +85,8 @@ class _MyHomePageState extends State<MyHomePage> {
       // Bottom Navigator Starts here!!
 
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0, // this will be set when a new tab is tapped
+         onTap: onTabTapped,
+        currentIndex: _currentIndex, // this will be set when a new tab is tapped
         items: [
           BottomNavigationBarItem(
             icon: new Icon(Icons.home),
