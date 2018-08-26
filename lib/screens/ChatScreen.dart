8357e09e9c0664cf2d334904522373197819e20b
Prabeen04 <prabeen.strange@gmail.com>
2018-08-26 22:46:13 +0530

    TextField and controller added

--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -2,12 +2,19 @@
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
+  final TextEditingController _textController;
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        appBar: AppBar(),
-        body: Text('Chat Screen')
+        appBar: AppBar(
+          title: Text('Chats')
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            controller: _textController,
+          )        ),
       ),
     );
   }
