19b82bcfd50ab2d05cf4c0f39a4bd8a9bdd13b5b
Prabeen04 <prabeen.strange@gmail.com>
2018-08-26 23:29:36 +0530

    small stuff

--- lib/widgets/MyHomePage.dart
+++ lib/widgets/MyHomePage.dart
@@ -14,95 +14,104 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(
-              Icons.notifications,
-              color: Colors.black38,
-            ),
-            tooltip: 'Notifications',
-            onPressed: () {
-              print('Notification Icon Pressed');
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => NotificationScreen()));
-            },
-          ),
-          new IconButton(
-            icon: new Icon(
-              Icons.settings,
-              color: Colors.black38,
-            ),
-            tooltip: 'Settings',
-            onPressed: () {
-              print('Settings Icon Pressed');
-            },
-          ),
-          new IconButton(
-            icon: new Icon(
-              Icons.account_circle,
-              color: Colors.black38,
-            ),
-            tooltip: 'Profile',
-            onPressed: () {
-              print('Settings Icon Pressed');
-            },
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
+        appBar: AppBar(
+          title: Text('Home'),
+          actions: <Widget>[
+            new IconButton(
+              icon: new Icon(
+                Icons.notifications,
+                color: Colors.black38,
               ),
-            ),
-            ListTile(
-              title: Text('Profile'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
+              tooltip: 'Notifications',
+              onPressed: () {
+                print('Notification Icon Pressed');
                 Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => ProfileScreen()));
+                    builder: (BuildContext context) => NotificationScreen()));
               },
             ),
-            ListTile(
-              title: Text('News'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => NewsScreen()));
+            new IconButton(
+              icon: new Icon(
+                Icons.settings,
+                color: Colors.black38,
+              ),
+              tooltip: 'Settings',
+              onPressed: () {
+                print('Settings Icon Pressed');
+              },
+            ),
+            new IconButton(
+              icon: new Icon(
+                Icons.account_circle,
+                color: Colors.black38,
+              ),
+              tooltip: 'Profile',
+              onPressed: () {
+                print('Settings Icon Pressed');
               },
             ),
           ],
         ),
-      ),
-      body: Center(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.network(
-              'https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg',
-            ),
-          ],
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                  currentAccountPicture: new CircleAvatar(
+                    backgroundImage: new NetworkImage(
+                        'https://i.ytimg.com/vi/xwa1cBWWCVY/hqdefault.jpg'),
+                  ),
+                  accountName: Text(
+                    "Prabeen",
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                  ),
+                  accountEmail: Text(
+                    "prabeen.strange@gmail.com",
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                  )),
+              ListTile(
+                title: Text('Profile'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) => ProfileScreen()));
+                },
+              ),
+              ListTile(
+                title: Text('News'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) => NewsScreen()));
+                },
+              ),
+            ],
+          ),
         ),
-      ),
+        body: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network(
+                'https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg',
+              ),
+            ],
+          ),
+        ),
 
-      // Bottom Navigator Starts here!!
+        // Bottom Navigator Starts here!!
 
-      // App level floating action Button!!
-      floatingActionButton: FancyFab() // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        // App level floating action Button!!
+        floatingActionButton:
+            FancyFab() // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
