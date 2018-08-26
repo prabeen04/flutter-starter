6715049fc9b2f3b9c7565416d6969f08eedf8fa4
Prabeen04 <prabeen.strange@gmail.com>
2018-08-26 23:09:01 +0530

    small stuff

--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
-  final TextEditingController _textController ;
+  // final TextEditingController _textController ;
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
-        appBar: AppBar(title: Text('Chats')),
-        body: Container(
-            padding: const EdgeInsets.all(20.0),
+        child: Scaffold(
+      appBar: AppBar(title: Text('Chats')),
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
             child: Column(
-              children: <Widget>[
-                TextField(
-                  keyboardType: TextInputType.text,
-                  controller: _textController,
-                  maxLines: null,
-                ),
-                IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: (){},
-                )
-              ],
-            )),
+          children: <Widget>[
+            TextField(
+              keyboardType: TextInputType.text,
+              // controller: _textController,
+              maxLines: null,
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () {},
+            )
+          ],
+        )),
       ),
-    );
+    ));
   }
 }
