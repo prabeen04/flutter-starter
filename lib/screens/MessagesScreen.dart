26ca155aaddb1d8718e591441afb7528d95ce3fd
Prabeen04 <prabeen.strange@gmail.com>
2018-08-27 20:31:53 +0530

    code cleaned

--- lib/screens/MessagesScreen.dart
+++ lib/screens/MessagesScreen.dart
@@ -16,24 +16,6 @@ Future fetchPost() async {
   }
 }
 
-class Post {
-  final int userId;
-  final int id;
-  final String title;
-  final String body;
-
-  Post({this.userId, this.id, this.title, this.body});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
-    );
-  }
-}
-
 class MessagesScreen extends StatefulWidget {
   @override
   _MessagesScreenState createState() => _MessagesScreenState();
