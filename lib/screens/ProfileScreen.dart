abfe985e63b2d8fa04b779dd1c6c0c7f38d410a9
Prabeen04 <prabeen.strange@gmail.com>
2018-08-29 23:19:18 +0530

    DefaultTabViewController added

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -16,7 +16,29 @@ class ProfileScreen extends StatelessWidget {
                 indicatorColor: Colors.white,
               ),
             ),
-            body: Text('kdjf'),
+            body: DefaultTabController(
+          length: 3,
+          child: new Scaffold(
+            appBar: new AppBar(
+              actions: <Widget>[],
+              title: new TabBar(
+                tabs: [
+                  new Tab(icon: new Icon(Icons.directions_car)),
+                  new Tab(icon: new Icon(Icons.directions_transit)),
+                  new Tab(icon: new Icon(Icons.directions_bike)),
+                ],
+                indicatorColor: Colors.white,
+              ),
+            ),
+            body: new TabBarView(
+              children: [
+                new Icon(Icons.directions_car,size: 50.0,),
+                new Icon(Icons.directions_transit,size: 50.0,),
+                new Icon(Icons.directions_bike,size: 50.0,),
+              ],
+            ),
+          ),
+        )
         );
   }
 }
