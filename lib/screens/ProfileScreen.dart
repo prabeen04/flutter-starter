4a2b182ecb1e6b13fba438860f3c72f77ec8730b
Prabeen04 <prabeen.strange@gmail.com>
2018-08-29 23:03:26 +0530

    Tab bar added

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -4,8 +4,25 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profile')),
-      body: Container(child: Center(child: Text('Profile Screen'))),
+      appBar: AppBar(
+        // title: Text('Profile'),
+        actions: <Widget>[],
+              title: new TabBar(
+                tabs: [
+                  new Tab(icon: new Icon(Icons.directions_car)),
+                  new Tab(icon: new Icon(Icons.directions_transit)),
+                  new Tab(icon: new Icon(Icons.directions_bike)),
+                ],
+                indicatorColor: Colors.white,
+              ),
+            ),
+            body: new TabBarView(
+              children: [
+                new Icon(Icons.directions_car,size: 50.0,),
+                new Icon(Icons.directions_transit,size: 50.0,),
+                new Icon(Icons.directions_bike,size: 50.0,),
+              ],
+        ),
     );
   }
 }
