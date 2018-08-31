4e1b6a285f0e4a8b8e3e560468a9c940c22bfaa4
Prabeen04 <prabeen.strange@gmail.com>
2018-08-31 21:03:10 +0530

    bosy added

--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -22,7 +22,51 @@ class _ChatScreenState extends State<ChatScreen> {
         child: Scaffold(
       resizeToAvoidBottomPadding: false,
       appBar: AppBar(title: Text('Chats')),
-      body: 
+      body: new Form(
+          key: this._formKey,
+          child: new ListView(
+            children: <Widget>[
+              new TextFormField(
+                keyboardType: TextInputType.emailAddress, // Use email input type for emails.
+                decoration: new InputDecoration(
+                  hintText: 'you@example.com',
+                  labelText: 'E-mail Address'
+                ),
+                validator: this._validateEmail,
+                onSaved: (String value) {
+                  this._data.email = value;
+                }
+              ),
+              new TextFormField(
+                obscureText: true, // Use secure text for passwords.
+                decoration: new InputDecoration(
+                  hintText: 'Password',
+                  labelText: 'Enter your password'
+                ),
+                validator: this._validatePassword,
+                onSaved: (String value) {
+                  this._data.password = value;
+                }
+              ),
+              new Container(
+                width: screenSize.width,
+                child: new RaisedButton(
+                  child: new Text(
+                    'Login',
+                    style: new TextStyle(
+                      color: Colors.white
+                    ),
+                  ),
+                  onPressed: this.submit,
+                  color: Colors.blue,
+                ),
+                margin: new EdgeInsets.only(
+                  top: 20.0
+                ),
+              )
+            ],
+          ),
+        )
     ));
   }
 }
