9a16a374eeff507a9649a47796dacf30d8b97ef6
Prabeen04 <prabeen.strange@gmail.com>
2018-09-01 20:10:59 +0530

    small stuff

--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -5,7 +5,7 @@ class ChatScreen extends StatefulWidget {
   // final TextEditingController _textController ;
   @override
   _ChatScreenState createState() {
-    return _ChatScreenState();
+    return new _ChatScreenState();
   }
 }
 
@@ -15,8 +15,8 @@ class _LoginData {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  _LoginData _data = _LoginData();
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  _LoginData _data = new _LoginData();
   String _validateEmail(String value) {
     // If empty value, the isEmail function throw a error.
     // So I changed this function with try and catch.
@@ -36,7 +36,6 @@ class _ChatScreenState extends State<ChatScreen> {
 
     return null;
   }
-
   void submit() {
     // First validate form.
     if (this._formKey.currentState.validate()) {
@@ -47,51 +46,57 @@ class _ChatScreenState extends State<ChatScreen> {
       print('Password: ${_data.password}');
     }
   }
-
   @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
+        final Size screenSize = MediaQuery.of(context).size;
 
     return Container(
         child: Scaffold(
-            resizeToAvoidBottomPadding: false,
-            appBar: AppBar(title: Text('Chats')),
-            body: Container(
-              margin: const EdgeInsets.all(20.0),
-                child: Form(
-              key: this._formKey,
-              child: ListView(
-                children: <Widget>[
-                  TextFormField(
-                      keyboardType: TextInputType
-                          .emailAddress, // Use email input type for emails.
-                      decoration: InputDecoration(labelText: 'E-mail Address'),
-                      validator: this._validateEmail,
-                      onSaved: (String value) {
-                        this._data.email = value;
-                      }),
-                  TextFormField(
-                      obscureText: true, // Use secure text for passwords.
-                      decoration:
-                          InputDecoration(labelText: 'Enter your password'),
-                      validator: this._validatePassword,
-                      onSaved: (String value) {
-                        this._data.password = value;
-                      }),
-                  Container(
-                    width: screenSize.width,
-                    child: RaisedButton(
-                      child: Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: this.submit,
-                      color: Colors.blue,
-                    ),
-                    margin: EdgeInsets.only(top: 20.0),
-                  )
-                ],
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(title: Text('Chats')),
+      body: new Form(
+          key: this._formKey,
+          child: new ListView(
+            children: <Widget>[
+              new TextFormField(
+                keyboardType: TextInputType.emailAddress, // Use email input type for emails.
+                decoration: new InputDecoration(
+                  labelText: 'E-mail Address'
+                ),
+                validator: this._validateEmail,
+                onSaved: (String value) {
+                  this._data.email = value;
+                }
               ),
-            ))));
+              new TextFormField(
+                obscureText: true, // Use secure text for passwords.
+                decoration: new InputDecoration(
+                  labelText: 'Enter your password'
+                ),
+                validator: this._validatePassword,
+                onSaved: (String value) {
+                  this._data.password = value;
+                }
+              ),
+              new Container(
+                width: screenSize.width,
+                child: new RaisedButton(
+                  child: new Text(
+                    'Login',
+                    style: new TextStyle(
+                      color: Colors.white
+                    ),
+                  ),
+                  onPressed: this.submit,
+                  color: Colors.blue,
+                ),
+                margin: new EdgeInsets.only(
+                  top: 20.0
+                ),
+              )
+            ],
+          ),
+        )
+    ));
   }
 }
